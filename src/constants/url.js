b4bfc30b299e4c12870e7a122623882e27a227eb
Izadora <71834989+Izadora02@users.noreply.github.com>
2022-10-05 01:23:39 -0300

    Add files via upload

---
+++ src/constants/url.js
@@ -0,0 +1,6 @@
+export const urlServer = 'https://prometeus-server.herokuapp.com/';
+
+//'http://localhost:5000/';
+//'https://prometeus-server.herokuapp.com/'
+
+export const urlClient = 'http://localhost:3000/';
